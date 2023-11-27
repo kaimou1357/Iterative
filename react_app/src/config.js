b4ad0404f5e930d8bebf74de01ff1908eabdf7c5
Kai Mou <kaimou1357@gmail.com>
2023-11-27 00:01:52 -0800

    Update React App

--- react_app/src/config.js
+++ react_app/src/config.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://0.0.0.0:8000/api';
+const API_BASE_URL = 'https://iterative-b9e7455b2ea4.herokuapp.com/api';
 
 export { API_BASE_URL };
