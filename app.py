0f3274106bf45b5ed80fb2f20a1ba79d969a9ed0
Kai Mou <kaimou1357@gmail.com>
2023-11-27 00:49:02 -0800

    Log OpenAI Error

--- app.py
+++ app.py
@@ -633,33 +633,8 @@ def generate():
           frequency_penalty=0,
           presence_penalty=0
       )
-    except openai.error.Timeout as e:
-      #Handle timeout error, e.g. retry or log
-      logging.debug(f"OpenAI API request timed out: {e}")
-      pass
-    except openai.error.APIError as e:
-      #Handle API error, e.g. retry or log
-      logging.debug(f"OpenAI API returned an API Error: {e}")
-      pass
-    except openai.error.APIConnectionError as e:
-      #Handle connection error, e.g. check network or log
-      logging.debug(f"OpenAI API request failed to connect: {e}")
-      pass
-    except openai.error.InvalidRequestError as e:
-      #Handle invalid request error, e.g. validate parameters or log
-      logging.debug(f"OpenAI API request was invalid: {e}")
-      pass
-    except openai.error.AuthenticationError as e:
-      #Handle authentication error, e.g. check credentials or log
-      logging.debug(f"OpenAI API request was not authorized: {e}")
-      pass
-    except openai.error.PermissionError as e:
-      #Handle permission error, e.g. check scope or log
-      logging.debug(f"OpenAI API request was not permitted: {e}")
-      pass
-    except openai.error.RateLimitError as e:
-      #Handle rate limit error, e.g. wait or log
-      logging.debug(f"OpenAI API request exceeded rate limit: {e}")
+    except Exception as e:
+      logging.debug(f"OpenAI API request failed: {e}")
       pass
 
     response_text = response.choices[0].message.content
