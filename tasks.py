4539d8416a0fb0a16ace6f9689a0a4a914256005
Kai Mou <kaimou1357@gmail.com>
2023-11-27 20:11:01 -0800

    Streaming Works

--- tasks.py
+++ tasks.py
@@ -17,5 +17,18 @@ def stream_gpt_response(model_name, messages, tokens_remaining):
           max_tokens=tokens_remaining,
           top_p=1,
           frequency_penalty=0,
-          presence_penalty=0
+          presence_penalty=0,
+          stream=True
       )
+    collected_chunks = []
+    collected_messages = []
+    # iterate through the stream of events
+    for chunk in response:
+        print(chunk)
+        collected_chunks.append(chunk)  # save the event response
+        chunk_message = chunk.choices[0].delta  # extract the message
+        collected_messages.append(chunk_message)  # save the message
+
+    # print the time delay and text received
+    full_reply_content = ''.join(["" if m.content is None else m.content for m in collected_messages])
+    print(full_reply_content)
