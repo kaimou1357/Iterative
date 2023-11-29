6b308dead080aab1665b77fcb1c36681ef629b3f
Kai Mou <kaimou1357@gmail.com>
2023-11-28 23:57:18 -0800

    Missed Typo

--- app.py
+++ app.py
@@ -134,8 +134,8 @@ migrate = Migrate(app, db)
 login_manager = LoginManager(app)
 bcrypt = Bcrypt(app)
 
-celery = Celery(app.name, broker_url=os.environ.get('REDIS_URL'),
-        result_backend=os.environ.get('REDIS_URL') )
+celery = Celery(app.name, broker_url=os.environ.get('REDISCLOUD_URL'),
+        result_backend=os.environ.get('REDISCLOUD_URL') )
 celery.conf.update(app.config)
 
 
