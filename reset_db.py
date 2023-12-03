c7d6bb9eb19f59fc6eb9275f148612f469590b72
Kai Mou <kaimou1357@gmail.com>
2023-12-03 14:51:32 -0800

    Cleaned

--- reset_db.py
+++
@@ -1,9 +0,0 @@
-from app import app, db
-
-print("Database URI:", app.config['SQLALCHEMY_DATABASE_URI'])
-
-with app.app_context():
-    # Drop all existing tables
-    db.drop_all()
-
-print("Database tables dropped successfully.")
