cfb4fd59b47725132da403d3c894e44dda774631
thooombs <thomaz639@gmail.com>
2023-12-21 22:23:21 -0300

    banner and page

--- next_app/src/app/components/banner.tsx
+++ next_app/src/app/components/banner.tsx
@@ -1,40 +1,62 @@
-import React from "react";
+'use client'
+
+import React, { useEffect } from "react";
 import pc from "../../../public/pc.jpg";
 import Image from "next/image";
 import Link from "next/link";
 import test from "../../../public/TBD.png";
+import Modal from "./modal";
 
 export default function banner() {
+
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      // Apply the animation classes here
+    }, 5000); // Restart every 5 seconds
+  
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
-    <div className="relative bg-black ">
-      <div className="md:py-0 py-48">
-        <div className="absolute inset-0 bg-gray-900 bg-opacity-80 flex flex-col items-center justify-center p-8">
-          <div className="flex flex-col md:flex-row  gap-10  ">
-            <div>
+    <div className=" bg-black h-auto">
+      <div className="">
+        <div className=" inset-0 bg-zinc-950 h-auto  bg-opacity-80 flex flex-col items-center justify-center">
+          <div className="flex flex-col  gap-10 py-10 sm:py-24 ">
+            <div className="text-center">
               <h2 className="text-5xl font-bold text-white">Iterative</h2>
-              <p className="h2 my-5 text-lg text-white">
+              <p className="   my-5 text-lg text-white">
                 Build amazing applications without writing a single line of
                 code.
               </p>
-              <Link
-                href="/tool"
-                className="bg-blue-500 text-white p-2 mr-4 rounded "
-              >
-                Try it out!
-              </Link>
+
               <Link
                 href="/auth"
                 className="bg-blue-500 text-white p-2 rounded "
               >
-                Sign up
+                Get Started!
               </Link>
             </div>
-            <Image width={450} height={450} alt="main" src={test}></Image>
+
+            
+            <div className="flex flex-col mt-5">
+              <div className="bg-gray-900 h-96 w-auto flex rounded ">
+                <div className=" flex flex-col  mx-10  justify-center items-center rounded overflow-hidden   animate-masking delay-[2000ms] ">
+                  <Modal />
+                </div>
+              </div>
+              <div className=" border border-white rounded-lg">
+                <div className="w-44 "><p className="overflow-hidden whitespace-nowrap border-r-4 border-r-white animate-typing  my-3 ml-3 text-lg text-white ">
+                  Make me a modal
+                </p></div>
+                
+              </div>
+            </div>
           </div>
         </div>
       </div>
 
-      <Image src={pc} width={1920} height={1080} alt="Picture of the author" />
+      {/* <Image src={pc} width={1920} height={1080} alt="Picture of the author" /> */}
     </div>
   );
 }
